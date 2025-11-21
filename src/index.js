1778c678fe4dceba895c32821c6fffe176ba7adb
Sheraz Khan <206346817+sherazkdev@users.noreply.github.com>
2025-11-21 15:12:46 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import app from "./app.js";
 /** MONGO CONNECTION */
 import Connection from "./Connections/MongoConnection/Connection.js";
 
-Connection.Connect().then( () => {
-    app.listen(process.env.PORT,() => console.log(`\n Project at running http://localhost:${process.env.PORT}`));
+Connection().then( () => {
+    // app.listen(process.env.PORT,() => console.log(`\n Project at running http://localhost:${process.env.PORT}`));
 }).catch( (connectionError) => new Error(connectionError));
