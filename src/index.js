25dd091e8ee659f70b79d2a55f5517d27cfe6256
Sheraz Khan <206346817+sherazkdev@users.noreply.github.com>
2025-11-21 15:37:48 +0500

    Export app as default from index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ import Connection from "./Connections/MongoConnection/Connection.js";
 Connection().then( () => {
     // app.listen(process.env.PORT,() => console.log(`\n Project at running http://localhost:${process.env.PORT}`));
 }).catch( (connectionError) => console.log(connectionError);
+
+export default app;
